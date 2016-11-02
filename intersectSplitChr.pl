961acfde0b5ee7ab11aa3330f59499390502c5b3
grenaud <gabriel.reno@gmail.com>
2016-11-02 15:26:34 +0100

    R script

--- intersectSplitChr.pl
+++ intersectSplitChr.pl
@@ -251,6 +251,12 @@ foreach my $chr (@humanchr){
   $cmd = $cmd. " | ".$bgzipcmd." -c > ".$outprefix."_".$chr.".mst.gz";
 
   $stringPrintlater=$stringPrintlater."\n".$outprefix."_".$chr.".mst.gz.tbi:\n\t".$cmd."\n\t".$tabixcmd."  -s 1 -b 2 -e 2 ".$outprefix."_".$chr.".mst.gz\n\n";
+
+  #add no undef
+  push(@arrayDeleteCombined, $outprefix."_".$chr.".noundef.mst.gz.tbi");
+
+  $stringPrintlater=$stringPrintlater."\n".$outprefix."_".$chr.".noundef.mst.gz.tbi:\n\t".$mistarfilter." ".$outprefix."_".$chr.".mst.gz | ".$bgzipcmd." -c > ".$outprefix."_".$chr.".noundef.mst.gz\n\t".$tabixcmd."  -s 1 -b 2 -e 2 ".$outprefix."_".$chr.".noundef.mst.gz\n\n";
+
 }
 
 
@@ -266,6 +272,22 @@ $stringPrintlater=$stringPrintlater."\n".$outprefix."_all.mst.gz.tbi: ".join(" "
 
 push(@arrayTargets,        $outprefix."_all.mst.gz.tbi");
 push(@arrayDeleteCombined, $outprefix."_all.mst.gz.tbi");
+
+
+
+
+
+#noundef
+my $cmdcat = $mistarcat." ";
+foreach my $chr (@humanchr){
+  $cmdcat = $cmdcat. " ".$outprefix."_".$chr.".noundef.mst.gz ";
+}
+$cmdcat = $cmdcat. " | ".$bgzipcmd." -c > ".$outprefix."_all.noundef.mst.gz";
+
+$stringPrintlater=$stringPrintlater."\n".$outprefix."_all.noundef.mst.gz.tbi: ".join(" ",@arrayDeleteCombined)."\n\t".$cmdcat."\n\t".$tabixcmd."  -s 1 -b 2 -e 2 ".$outprefix."_all.noundef.mst.gz\n\n";
+
+push(@arrayTargets,        $outprefix."_all.noundef.mst.gz.tbi");
+push(@arrayDeleteCombined, $outprefix."_all.noundef.mst.gz.tbi");
 
 
 print "SHELL := /bin/bash\n\nall:\t".join(" ",@arrayTargets)."\n\nclean:\n\trm -vf ".join(" ",@arrayTargets)."\n\ncleancombined:\n\trm -vf ".join(" ",@arrayDeleteCombined)."\n\n".$stringPrintlater."\n";
